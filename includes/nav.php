390802c042c4fa737887401109b85ade0cce2fcc
Anshu Raj <anshur0202@gmail.com>
2021-06-09 09:40:11 +0530

    first commit

---
+++ includes/nav.php
@@ -0,0 +1,25 @@
+<header id="header" class="fixed-top">
+    <div class="container d-flex align-items-center">
+
+      <h1 class="logo me-auto"><a href="index.html">Medilab</a></h1>
+      <!-- Uncomment below if you prefer to use an image logo -->
+      <!-- <a href="index.html" class="logo me-auto"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+          <li><a class="nav-link scrollto active" href="index.html">Home</a></li>
+          <li><a class="nav-link scrollto" href="#about">About</a></li>
+          <li><a class="nav-link scrollto" href="symptom.html">Symptoms</a></li>
+          <li><a class="nav-link scrollto" href="#equipments">Equipments</a></li>
+          <li><a class="nav-link scrollto" href="results.html" >Tests</a></li>
+          
+          
+          <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+
+      <a href="" class="appointment-btn scrollto"><span class="d-none d-md-inline">Register</a>
+
+    </div>
+  </header>
